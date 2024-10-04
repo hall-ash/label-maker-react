79223a67e4fbbe12eb3c0e1a9a337c5330b6b1dd
ahall <ahall@inventprise.com>
2024-10-04 16:05:56 -0700

    add old version of SettingsModal

---
+++ src/SettingsModalOld.js
@@ -0,0 +1,174 @@
+import React, { useState, useContext } from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button, Input, InputGroup, InputGroupText, FormGroup, Label as RSLabel } from 'reactstrap';
+import { settingsSchema, getErrors } from './validationSchemas.js';
+
+const SettingsModal = ({ isOpen, toggle }) => {
+
+  const { savedSettings, setSavedSettings } = useContext(SettingsContext);
+
+  const [settings, setSettings] = useState(savedSettings);
+  const [errors, setErrors] = useState({}); 
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  // useEffect(() => {
+  //   if (isOpen) {
+  //     const defaultSettings = {
+  //       'hasBorder': false, 
+  //       'fontSize': 12, 
+  //       'padding': 1.75,
+  //       'fileName': 'labels',
+  //     }
+  //     const savedSettings = JSON.parse(localStorage.getItem('settings'));
+  //     if (savedSettings) {
+  //       const newSavedSettings = Object.keys(defaultSettings).reduce((acc, setting) => {
+  //         const validSetting = savedSettings[setting] !== undefined && savedSettings[setting] !== "";
+  //         acc[setting] = validSetting ? savedSettings[setting] : defaultSettings[setting];
+  //         return acc;
+  //       }, {});
+  //       setSettings(newSavedSettings);
+  //     } else {
+  //       setSettings(defaultSettings);
+  //     }
+  //   } 
+  // }, [isOpen]);
+
+
+
+  // const handleSaveClick = () => {
+
+  //   try {
+      
+  //     if (!Object.keys(errors).length) {
+  //       setIsSubmitting(true);
+  //       toggle();
+
+  //       const changedSettings = Object.fromEntries(
+  //         Object.entries(settings).filter(
+  //           ([key, value]) => key in defaultSettings && value !== defaultSettings[key]
+  //         )
+  //       );
+        
+  //       if (Object.keys(changedSettings).length) {
+  //         localStorage.setItem('settings', JSON.stringify(changedSettings));
+  //       } else {
+  //         localStorage.removeItem('settings');
+  //       }
+  //     } 
+  //     setIsSubmitting(false);
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+
+  // };
+ 
+
+  const handleSaveClick = () => {
+    setSavedSettings(settings);
+  };
+
+  const handleBorderToggle = () => {
+    setSettings(prevSettings => ({
+      ...prevSettings,
+      hasBorder: !prevSettings.hasBorder,
+    }));
+  };
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    console.log('name', name, 'value', value);
+
+    setSettings(prevSettings => {
+      const updatedSettings = {
+          ...prevSettings,
+          [name]: value,
+      };
+
+      console.log('settings', settings);
+
+      console.log('updatedSettings', updatedSettings);
+
+      const parsedData = settingsSchema.safeParse(updatedSettings);
+      console.log('parsedData', parsedData);
+      setErrors(getErrors(parsedData.error));
+
+      return updatedSettings;
+    });
+  };
+
+  return (
+    <Modal isOpen={isOpen} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Label Settings</ModalHeader>
+      <ModalBody>
+        <FormGroup>
+          <RSLabel className="padding" for="padding">Padding</RSLabel>
+          <Input
+            id="padding"
+            name="padding"
+            type="number"
+            value={settings.padding}
+            onChange={handleChange}
+            min="0"
+            bsSize="sm"
+            className="label-padding-input"
+          />
+          {errors.padding && <small className="text-danger">{errors.padding}</small>}
+        </FormGroup>
+
+        <FormGroup>
+          <RSLabel className="fontSize" for="fontSize">Font Size</RSLabel>
+          <Input
+            id="fontSize"
+            name="fontSize"
+            type="number"
+            value={settings.fontSize}
+            onChange={handleChange}
+            min="1"
+            bsSize="sm"
+            className="label-font-size-input"
+          />
+          {errors.fontSize && <small className="text-danger">{errors.fontSize}</small>}
+        </FormGroup>
+
+        <FormGroup check className="d-flex align-items-center form-check-group">
+          <Input
+            type="checkbox"
+            id="border"
+            name="border"
+            checked={settings.hasBorder}
+            onChange={handleBorderToggle}
+            className="form-check-input"
+          />
+          <RSLabel htmlFor="border" check className="form-check-label ms-2">
+            Add Border
+          </RSLabel>
+        </FormGroup>
+
+        <FormGroup>
+          <RSLabel for="fileName" className="font-weight-bold">Save pdf as...</RSLabel>
+          <InputGroup>
+            <Input
+              id="fileName"
+              name="fileName"
+              type="text"
+              value={settings.fileName}
+              onChange={handleChange}
+              bsSize="sm"
+            />
+            <InputGroupText>.pdf</InputGroupText>
+          </InputGroup>
+          {errors.fileName && <small className="text-danger">{errors.fileName}</small>}
+        </FormGroup>
+      </ModalBody>
+
+      <ModalFooter className="justify-content-center">
+        <Button color="secondary" onClick={toggle}>
+          Cancel
+        </Button>{' '}
+        <Button color="primary" onClick={handleSaveClick} disabled={isSubmitting}>Save</Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default SettingsModal;
