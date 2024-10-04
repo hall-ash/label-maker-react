78c0557a52e4131b2887613240675c70a2cb43ef
ahall <ahall@inventprise.com>
2024-10-04 16:07:41 -0700

    add SettingsContext

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, { createContext, useState, useRef } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Navigation from './Navigation';
+import useLocalStorage from './useLocalStorage';
+
+const SettingsContext = createContext(null);
 
 const App = () => {
 
+  const defaultSettings = {
+    'hasBorder': false, 
+    'fontSize': 12, 
+    'padding': 1.75,
+    'fileName': 'labels',
+  };
+
+  const [localStorageSettings, setLocalStorageSettings] = useLocalStorage('LabelSettings', defaultSettings);
+  const [savedSettings, setSavedSettings] = useState(localStorageSettings);
+
+  const handleBeforeUnload = () => {
+    setLocalStorageSettings(savedSettings); // Save the current settings before unload
+  };
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []); // No dependencies, runs once when the component mounts
+
+
   return (
-    <Router>
-      <div>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <SettingsContext.Provider value={{ savedSettings, setSavedSettings }}>
+      <Router>
+        <div>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </SettingsContext.Provider>
   );
 };
 
