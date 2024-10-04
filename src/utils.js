01a2c03cdf95714c95f9cdd20331a99a63e3c1e2
ahall <ahall@inventprise.com>
2024-10-04 16:07:17 -0700

    add util functions

---
+++ src/utils.js
@@ -0,0 +1,5 @@
+String.prototype.toTitleCase = () => {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
+
+export { toTitleCase };
