d6cbfb3e442ce491b2b7c3cade3136f4b86e8cfd
hallash <88917636+hall-ash@users.noreply.github.com>
2024-10-05 18:19:42 -0700

    remove SettingsContext, localStorage, move to SettingsModal

--- src/App.js
+++ src/App.js
@@ -1,47 +1,13 @@
 // import logo from './logo.svg';
-import React, { createContext, useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Navigation from './Navigation';
-import useLocalStorage from './useLocalStorage';
-import SettingsContext from './SettingsContext';
-import _ from 'lodash';
 
 const App = () => {
 
-  const defaultSettings = {
-    'hasBorder': false, 
-    'fontSize': 12, 
-    'padding': 1.75,
-    'fileName': 'labels',
-  };
-
-  const [localStorageSettings, setLocalStorageSettings] = useLocalStorage('LabelSettings', defaultSettings);
-  const [savedSettings, setSavedSettings] = useState(localStorageSettings);
-
-  const handleBeforeUnload = (e) => {
-    // check to see if savedSettings have changed
-    console.log('savedSettings', savedSettings);
-    if (! _.isEqual(localStorageSettings, savedSettings)) {
-      setLocalStorageSettings(savedSettings); // Save the current settings before unload
-    }
-
-    e.preventDefault(); // Prevent the default unload action
-    e.returnValue = ''; // Required for showing the confirmation dialog
-  };
-
-  useEffect(() => {
-    window.addEventListener('beforeunload', handleBeforeUnload);
-    
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
-    };
-  }, []); // No dependencies, runs once when the component mounts
-
-
   return (
-    <SettingsContext.Provider value={{ savedSettings, setSavedSettings }}>
       <Router>
         <div>
           <Navigation />
@@ -51,7 +17,6 @@ const App = () => {
           </Routes>
         </div>
       </Router>
-    </SettingsContext.Provider>
   );
 };
 
