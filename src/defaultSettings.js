fda280a144ff4b15e0dea1404303539e9814b679
hallash <88917636+hall-ash@users.noreply.github.com>
2024-10-07 07:27:40 -0700

    separate constants

---
+++ src/defaultSettings.js
@@ -0,0 +1,15 @@
+const defaultSettings = {
+  'hasBorder': false, 
+  'fontSize': 12, 
+  'padding': 1.75,
+  'fileName': 'labels',
+};
+
+const labelSheetTypes = [
+  "LCRY-1700", // / RNBW-2200, label size: 33mm x 13mm, 17 rows x 5 cols",
+  "size 1",
+  "size 2",
+  "size 3",
+];
+
+export { defaultSettings, labelSheetTypes } ;
