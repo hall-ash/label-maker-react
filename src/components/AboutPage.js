48a0d8bd7d8ed73d66821df0eef1163be2bc39e8
ahall <ahall@inventprise.com>
2025-02-16 16:42:41 -0800

    modify about pg

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -7,7 +7,7 @@ const AboutPage = () => {
   return (
     <Container className='col-md-7'>
       <h1>About</h1>
-      <p>put helpful info here</p>
+      <p>teams me for questions</p>
     </Container>
   );
 }
